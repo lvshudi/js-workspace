7682d73b8db2bee08eaa0c9de00a41d4e92bd020
lvshudi <lvshudi119@126.com>
2019-08-20 18:48:40 +0800

    wanchengpin

--- main.js
+++ main.js
@@ -19,7 +19,18 @@ function printMultiplyTable(multiplyArray) {
 }
 
 function generateMultiplyArray(firstNumber, secondNumber) {
-
+    var generatedArray = [];
+    var times = secondNumber = firstNumber + 1;
+    for (let rowIndex = 0; rowIndex < times; index++) {
+        //row
+        var row = [];
+        for (let colums = 0; colums < (rowIndex + 1); colums++) {
+            var result = (colums + firstNumber) * (rowIndex + firstNumber);
+            row.push(result);
+        }
+        generatedArray.push(row);
+    }
+    return generatedArray;
 }
 
 module.exports = {
